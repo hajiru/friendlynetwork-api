f8ddb538af0307f4aa7c1ec12dee3989ec2dd977
dabyn <davyn.inthavong@gmail.com>
2023-06-25 15:04:12 -0500

    created models

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+var moment = require('moment');
+const reactionSchema = require('./Reaction'); 
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlength: 1,
+        },
+        createdAt: {
+            type: String,
+            default: moment().format("MMMM Do, YYYY [at] h:mm a"),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions:[reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false
+
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
